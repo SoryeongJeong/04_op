b8526f1426fa4605f421326f351c90c6bc046d9a
SoryeongJeong <55128033+SoryeongJeong@users.noreply.github.com>
2019-10-01 15:04:17 +0900

    04_op

--- main.c
+++ main.c
@@ -4,16 +4,51 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int x;
-	int y;
 	
-	printf("input two integers : ");
-	scanf("%d %d", &x, &y );
+/* project1 */
 	
-	printf("+ result is %d\n", x + y);
-	printf("- result is %d\n", x-y);
-	printf("* result is %d\n", x * y);
-	printf("/ result is %d\n", x/y);
-	printf("% result is %d\n", x%y);
-	return 0;
-}
+		int x, y, z, m;
+		int a,b,c;
+	
+		x=1;
+		z=2; 
+		a=3;
+		b=4;
+		c=5;
+	
+		y= a*x*x + b*x + c;
+		m = (x+y+z)/3;
+	
+		printf("y=%d, m=%d\n", y, m);
+	
+	
+
+	/*project2 */
+
+	
+		int k=3;
+		int cond1, cond2;
+	
+		cond1 = (2<k) && (k<5);
+		cond2 = (k>1)||(k<4)&&(k>3);
+	
+		printf("cond1 = %d, cond2 = %d\n", cond1, cond2);
+	
+
+
+	/*project 3 */
+	
+
+		int s, l;
+		printf("input two integers : ");
+		scanf("%d %d", &s,&l);
+		
+		printf("+ result if : %d\n", s+l);
+		printf("- result if : %d\n", s-l);
+		printf("* result if : %d\n", s*l);
+		printf("/ result if : %d\n", s/l);
+		printf("% result if : %d\n", s%l);
+	
+		return 0;
+	}
+
